152f123000bcbf8453beaf63f3e1c9f5a955ff8d
valerylot <lotvalery@gmail.com>
2024-05-08 16:04:18 -0700

    another solution for valid anagram

--- validAnagram.js
+++ validAnagram.js
@@ -18,3 +18,38 @@ var isAnagram = function(s, t) {
     }
     return true;
 };
+
+//SOLUTION 2
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length != t.length) return false
+
+    let map = {}
+
+    for(let i of s){
+        if(!map[i]){
+            map[i] = 1
+        } else{
+            map[i]++
+        }
+    }
+
+    for(let i of t){
+        if(!map[i]){
+            return false
+        }
+
+        if(map[i]){
+            map[i]--
+        }
+        if(map[i] === 0){
+            delete map[i]
+        }
+    }
+    return Object.keys(map).length === 0
+};
